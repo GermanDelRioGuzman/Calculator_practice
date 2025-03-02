e8cd2dc3228609fce0388aea3f888ba6a48de432
German avelino Del rio guzman <germanavelinodelrioguzman@Germans-MacBook-Air.local>
2025-03-01 20:39:38 -0600

    2

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-function calculate() {
-    // Obtener los valores de los inputs y convertirlos a números
-    const num1 = parseFloat(document.getElementById("num1").value) || 0;
-    const num2 = parseFloat(document.getElementById("num2").value) || 0;
-
-    // Calcular la suma
-    let result = num1 + num2;
-
-    // Mostrar el resultado
-    document.getElementById("result").innerText = "Result: " + result.toFixed(2);
-}
-
-function resetFields() {
-    document.getElementById("num1").value = "";
-    document.getElementById("num2").value = "";
-    document.getElementById("result").innerText = "Result: ";
-}
-=======
 const express = require("express");
 const path = require("path");
 
@@ -35,4 +16,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
->>>>>>> 0d19ff7 (second)
